a4a5988f7bfc7eaad3d7b64316054cbedea127e3
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2024-08-03 12:22:22 +0530

    adding major-project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <Row>
           <Col md={6} className="text-md-left">
             <h1>Welcome to HireCheck!</h1>
-            <p>Your gateway to finding the perfect job123.</p>
+            <p>Your gateway to finding the perfect job123 and added.</p>
           </Col>
           <Col md={6} className="text-md-right">
             <Image src={profileImage} roundedCircle />
