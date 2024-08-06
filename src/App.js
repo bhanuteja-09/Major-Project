0f17141298253d7b98255a7e3346232a2e66e00f
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2024-08-06 15:55:49 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import FindAJob from './pages/FindAJob';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import './App.css'; // Import global CSS
+import Header from './components/Header';
 
-// const DebugLocation = () => {
-//   const location = useLocation();
-//   console.log('Current URL:', location.pathname);
-//   return null;
-// };
+// import Login from './components/Login';
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router>
       <Header />
-      {/* <DebugLocation /> */}
+      <Home />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/find-a-job" element={<FindAJob />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/find-a-job" element={<FindAJob />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
